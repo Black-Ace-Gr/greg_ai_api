e790331a77dfec42eb3f3208fd4c8b100f14fc97
Black-Ace-Gr <gregorydavid373@gmail.com>
2026-08-21 20:13:31 +0300

    adding database

--- videostudio/settings.py
+++ videostudio/settings.py
@@ -1,4 +1,7 @@
 import os
+
+import dj_database_url
+
 """
 Django settings for videostudio project.
 
@@ -24,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uyxbf4-43_vd2$196y#(ow2j9unjpzqx9nl_znk-a)_6r8q^r#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+DATABASE_URL = "postgresql://neondb_owner:npg_IEdmaJ5Rui6t@ep-ancient-scene-ay5j1o5d-pooler.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 ALLOWED_HOSTS = ["*"]
 
@@ -86,10 +90,9 @@ WSGI_APPLICATION = 'videostudio.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    )
 }
 
 
